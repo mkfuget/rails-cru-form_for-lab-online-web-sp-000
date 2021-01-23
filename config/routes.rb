60e0530db4531de13e0a527136698e701d3f8e8d
Michael Fuget <michaelfuget@gmail.com>
2021-01-23 03:52:12 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'song/new'
+
+  get 'song/create'
+
+  get 'song/edit'
+
+  get 'song/update'
+
+  get 'song/destroy'
+
+  get 'song/index'
+
+  get 'song/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
